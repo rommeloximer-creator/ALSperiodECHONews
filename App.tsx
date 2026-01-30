854f7a7c8f2810a24b47876968366702f39b28f7
rommeloximer-creator <rommeloximer@gmail.com>
2026-01-30 11:50:35 +0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -77,6 +77,7 @@ useEffect(() => {
       <main>
         {activeCategory === 'HEADLINE' && (
         <>
+          <>
           <Hero 
             settings={settings} 
             featuredArticle={articles[0] || null} 
